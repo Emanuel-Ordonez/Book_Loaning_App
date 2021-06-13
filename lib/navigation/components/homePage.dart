d1fb8b72080190801818489ea3e2212795d96306
Regulator1904 <ordonezemanuel11@gmail.com>
2021-06-13 08:51:34 -0700

    somethin

--- lib/navigation/components/homePage.dart
+++ lib/navigation/components/homePage.dart
@@ -22,11 +22,48 @@ Widget cardview() {
             ),
           ]
         ),
-        Image.asset(
-          'assets/images/loginImage.jpg',
-          width: double.maxFinite,
-          height: 150,
-          fit: BoxFit.cover,
+        Row(
+          children: [
+            Expanded(
+              child: Image.asset(
+                'assets/images/loginImage.jpg',
+                width: double.maxFinite,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ]
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Icon(
+                Icons.favorite_border,
+                color: Colors.pink,
+                size: 24.0,
+                semanticLabel: 'label to announce',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Icon(
+                Icons.send_sharp,
+                color: Colors.black,
+                size: 24.0,
+              ),
+            ),
+          ] 
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Text(
+                'description will go here'
+              ),
+            ),
+          ]
         ),
       ]
     ),
