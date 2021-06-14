b85f11af7167bab44ac80c957ff47dc3d7c7fdcd
Regulator1904 <ordonezemanuel11@gmail.com>
2021-06-13 18:46:49 -0700

    added scrolling as well as limits on post

--- lib/navigation/components/homePage.dart
+++ lib/navigation/components/homePage.dart
@@ -1,71 +1,74 @@
 import 'package:flutter/material.dart';
 
 Widget homePage() {
-  return Column(
-    children: [
-      cardview(),
-      cardview(),
-    ]
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        cardview(),
+        cardview(),
+        cardview(),
+    ])
   );
 }
 
 Widget cardview() {
   return Container(
     padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-    child: Column(
-      children: [
-        Row(
+    child: Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(5),
+        child: Row(
           children: [
-            Text(
-              'username',
-              style: TextStyle(fontSize: 20),
-            ),
-          ]
+          Text(
+            'username',
+            style: TextStyle(fontSize: 20),
+          ),
+        ]),
+      ),
+      Container(
+        constraints: BoxConstraints(
+          minHeight: 100,
+          minWidth: double.infinity,
+          maxHeight: 400,
         ),
-        Row(
-          children: [
-            Expanded(
-              child: Image.asset(
-                'assets/images/loginImage.jpg',
-                width: double.maxFinite,
-                height: 150,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ]
+        child: Image.asset(
+          'assets/images/loginImage.jpg',
+          fit: BoxFit.fitWidth,
         ),
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: Icon(
-                Icons.favorite_border,
-                color: Colors.pink,
-                size: 24.0,
-                semanticLabel: 'label to announce',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: Icon(
-                Icons.send_sharp,
-                color: Colors.black,
-                size: 24.0,
-              ),
-            ),
-          ] 
+      ),
+      Row(children: [
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Icon(
+            Icons.favorite_border,
+            color: Colors.black,
+            size: 26.0,
+            semanticLabel: 'label to announce',
+          ),
         ),
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: Text(
-                'description will go here'
-              ),
-            ),
-          ]
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Icon(
+            Icons.insert_comment_outlined,
+            color: Colors.black,
+            size: 24.0,
+          ),
         ),
-      ]
-    ),
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Icon(
+            Icons.send_sharp,
+            color: Colors.black,
+            size: 24.0,
+          ),
+        ),
+      ]),
+      Row(children: [
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Text('description will go here'),
+        ),
+      ]),
+    ]),
   );
 }
